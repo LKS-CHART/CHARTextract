5f1f0d32066d03804ec9dba37520f56e5b557ea1
shaun-mathew <shaunmathew1@hotmail.com>
2018-02-16 17:57:21 -0500

    Moved ngram string functions into util

--- ngram/base_ngram.py
+++ ngram/base_ngram.py
@@ -1,10 +1,16 @@
+from util.string_functions import split_string_into_sentences, split_string_into_words
+
 class Ngram(object):
-    def __init__(self, text, n):
+    def __init__(self, text, n, normalize_frequency=False, distance_weighting=False):
         self.text = text
         self.n = n
-        self._textlen = None
         self.ngram_to_frequency = None
         self.ngram_to_matches = None
+        self._normalize_frequency = normalize_frequency
+        self._distance_weighting = distance_weighting
+        self.words = split_string_into_words(self.text)
+        self.sentences = split_string_into_sentences(self.text)
+        self._textlen = len(self.words)
 
     def top_k_ngrams(self, k):
         pass
@@ -25,12 +31,21 @@ class Ngram(object):
         pass
 
     def _calculate_frequency(self):
-        pass
+        if self._distance_weighting:
+            pass
+        else:
+            for i in range(self._textlen - self.n):
+                ngram = " ".join(self.words[i:i+self.n])
+                if ngram in self.ngram_to_frequency:
+                    self.ngram_to_frequency[ngram] += 1
+                else:
+                    self.ngram_to_frequency = 1
+
+        if self._normalize_frequency:
+            num_ngrams = sum(self.ngram_to_frequency.values())
+            self.ngram_to_frequency = {k: v/num_ngrams for k, v in self.ngram_to_frequency.items()}
 
     def _determine_match_locs(self):
-        pass
-
-    def _split_string(self):
         pass
 
 
---
+++ util/string_functions.py
@@ -0,0 +1,5 @@
+def split_string_into_sentences(text):
+    pass
+
+def split_string_into_words(text):
+    return text.split(".")
