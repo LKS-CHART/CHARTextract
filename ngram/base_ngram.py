7feebc1bc71a899b199b3714d4ec855c1cf371c5
shaun-mathew <shaunmathew1@hotmail.com>
2018-02-16 17:00:03 -0500

    Added Ngram class

---
+++ ngram/base_ngram.py
@@ -0,0 +1,34 @@
+class Ngram(object):
+    def __init__(self, text, n):
+        self.text = text
+        self.n = n
+        self.ngram_to_frequency = None
+        self.ngram_to_matches = None
+
+    def top_k_ngrams(self, k):
+        pass
+
+    def remove_n_grams(self, ngrams):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __and__(self, other):
+        pass
+
+    def calculate_logistics(self):
+        pass
+
+    def _calculate_frequency(self):
+        pass
+
+    def _determine_match_locs(self):
+        pass
+
+    def _split_string(self):
+        pass
+
