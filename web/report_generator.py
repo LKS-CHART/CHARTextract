60fe94664f5024ada08b04c576537c9a084741a3
shaun-mathew <shaunmathew1@hotmail.com>
2018-02-27 16:43:45 -0500

    Added matches argument to generate_report function

--- web/report_generator.py
+++ web/report_generator.py
@@ -2,5 +2,5 @@ import os
 from jinga2 import Environment, FileSystemLoader
 import re
 
-def generate_report(ids, class_names, failures, accuracy, output_folder, highlight_regexes=True):
+def generate_report(ids, class_names, failures, accuracy, matches, output_folder, highlight_regexes=True):
     pass
