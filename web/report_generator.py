cdab68e44cbef911fba826f627f3e16b9a632c5e
shaun-mathew <shaunmathew1@hotmail.com>
2018-02-28 18:11:55 -0500

    Added option for custom colours

--- web/report_generator.py
+++ web/report_generator.py
@@ -11,21 +11,25 @@ def generate_hsl_colour_dictionary(keys):
 
     return colour_dict
 
-def generate_error_report(output_directory, html_output_filename, html_template, variable_name, class_names, failures_dict, highlight_regexes=True):
+def generate_error_report(output_directory, html_output_filename, html_template, variable_name, class_names, failures_dict,
+                          highlight_regexes=True, custom_class_colours=None):
     json_filename = html_output_filename.split('.')[0] + '.json'
     generate_generic_report(output_directory, html_output_filename, html_template)
     generate_error_json(output_directory, json_filename, variable_name, class_names, failures_dict, highlight_regexes)
 
-def generate_error_json(output_directory, json_filename, variable_name, class_names, failures_dict, highlight_regexes=True):
+def generate_error_json(output_directory, json_filename, variable_name, class_names, failures_dict,
+                        highlight_regexes=True, custom_class_colours=None):
+
     json_fname = os.path.join(output_directory, json_filename)
 
     data = {}
 
     data['var_name'] = variable_name
-    data['classes'] = generate_hsl_colour_dictionary(class_names)
+    data['classes'] = generate_hsl_colour_dictionary(class_names) if not custom_class_colours else custom_class_colours
+    data['patient_cases'] = {}
 
     with open(json_fname, 'w') as outfile:
-        json.dumps(data, outfile)
+        json.dump(data, outfile, indent=4)
 
 def generate_generic_report(output_directory, html_output_filename, html_template):
     html_fname = os.path.join(output_directory, html_output_filename)
