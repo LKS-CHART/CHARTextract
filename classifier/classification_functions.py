3d94febc6a3947fdd1b958e14e795626fa8df906
shaun-mathew <shaunmathew1@hotmail.com>
2018-04-16 23:00:52 -0400

    Added min_classify and sputum_classify barebones

--- classifier/classification_functions.py
+++ classifier/classification_functions.py
@@ -1,2 +1,8 @@
 def max_classify(class_matches, class_captures, class_capture_scores, negative_label="None"):
     return max(class_capture_scores, key=int) if class_capture_scores else negative_label
+
+def min_classify(class_matches, class_captures, class_capture_scores, negative_label="None"):
+    return min(class_capture_scores, key=int) if class_capture_scores else negative_label
+
+def sputum_classify(class_matches, class_captures, class_capture_scores, negative_label="None"):
+    pass
