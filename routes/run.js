db7ca3755ce7a3263c4f656f73b9b7fc924f6db2
Sabrewarrior <Sabre8463>
2018-05-28 18:02:45 -0400

    Fixed run button

--- routes/run.js
+++ routes/run.js
@@ -3,40 +3,21 @@ var router = express.Router();
 var pyshell_connector = require("..\\python_connector\\connector");
 /* Save resource as json */
 
-
 pyshell = new pyshell_connector('__main__.py', {
     mode: 'json'
 }, function (err, results){
     console.log(results)
 });
 
-let x = 0;
-
-let test = function(message) {
-    if (message['status'] === 200){
-        resolve(message);
-    } else {
-        reject("404");
-    }
-};
-let runPromise = function() {
-    pyshell.on('message', message => test(message));
-    return new Promise((resolve, reject) => {
-
-    })
-};
-
-
 router.get('/:variable', function(req, res, next) {
     console.time("dbsave");
     console.log(req.params);
 
-
     pyshell.send({'function': 'run_variable', 'params': req.params});
-
-
-    x = x + 1;
-    Promise.all([runPromise]).then(function (message) {
+    var curPromise = new Promise(function (resolve, reject) {
+        pyshell.on('message', function(message) {resolve(message)});
+    });
+    Promise.all([curPromise]).then(function (result) {
         res.redirect("http://localhost:8080/NgramRegexNLP/generated_data/diagnosis/train/index.html");
     });
 });
