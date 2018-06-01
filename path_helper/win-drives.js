e607f7040032d6b376df08b94d496369d0480466
Sabrewarrior <Sabre8463>
2018-06-01 14:19:55 -0400

    stuff

--- path_helper/win-drives.js
+++ path_helper/win-drives.js
@@ -1,34 +1,7 @@
 const childProcess = require('child_process');
 const tableParser = require('table-parser');
-
+const path = require('path');
 const command = 'wmic logicaldisk get caption';
-
-function removeUsedLetters(usedLetters) {
-    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    for (let i = 0; i < usedLetters.length; i++) {
-        letters.splice(letters.indexOf(usedLetters[i]), letters.indexOf(":"));
-    }
-
-    return letters;
-}
-
-function letters() {
-    return new Promise((resolve, reject) => {
-        childProcess.exec(command, (err, stdout) => {
-            if (err) {
-                return reject(err);
-            }
-
-            const letters = tableParser.parse(stdout).map((caption) => {
-                return caption.Caption[0];
-            });
-
-            resolve(removeUsedLetters(letters));
-        });
-    });
-}
-
-module.exports.letters = letters;
 
 function usedLetters() {
     return new Promise((resolve, reject) => {
--- routes/path.js
+++ routes/path.js
@@ -2,43 +2,115 @@ let express = require('express');
 let router = express.Router();
 let fs = require('fs');
 let path = require('path');
-let baseDir = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+const os = require('os');
 let winDrives = require('../path_helper/win-drives');
 
-console.log(winDrives.usedLettersSync());
-console.log(fs.readdirSync("Z:\\"));
-/* GET home page. */
-router.post('/', function(req, res, next) {
-    res_json = { "filename": "Current Folder",
-                "Root": "Full\\Path\\to\\Folder",
-                "Type": "Folder",
-                "Children": [
-                    {
-                        "filename": "Child of Current Folder",
-                        "Root": "Full\\Path\\to\\Folder\\Child of Current Folder",
-                        "Type": "File",
-                        "Children": []
-                    }
 
-                ]};
-     res.send(JSON.stringify(res_json));
+function promisify(fn) {
+    /**
+     * @param {...Any} params The params to pass into *fn*
+     * @return {Promise<Any|Any[]>}
+     */
+    return function promisified(...params) {
+        return new Promise((resolve, reject) => fn(...params.concat([(err, ...args) => err ? reject(err) : resolve( args.length < 2 ? args[0] : args )])))
+    }
+}
+
+/* GET home page. */
+function get_directory(tokenized_path){
+    cur_path = path.resolve(path.join(...tokenized_path.slice(1)));
+    if (tokenized_path.length === 1){
+        let childDirs = winDrives.usedLetters();
+        return childDirs.then(function (result){
+            let arr = [];
+            result.forEach(file_name => {
+                 arr.push({
+                     "filename": file_name,
+                     "Root": ["Computer"],
+                     "Type": "Folder",
+                     "Children": []
+                 });
+            });
+            return arr;
+        }).catch(function(error){
+            console.log("Error reading " + cur_path + " : " + error);
+            return "Error reading " + cur_path + " : " + error;
+        })
+    } else {
+        if (!fs.statSync(cur_path).isDirectory()){
+            return [];
+        }
+        let childDirs = promisify(fs.readdir);
+        return childDirs(cur_path).then(function (result){
+            let arr = [];
+            result.forEach(file_name => {
+                try {
+                arr.push({
+                    "filename": file_name,
+                    "Root": tokenized_path,
+                    "Type": fs.statSync(path.join(cur_path, file_name)).isDirectory() ? "Folder" : "File",
+                    "Children": []
+                });} catch(err) {
+                    console.log(err);
+                }
+            });
+            return arr;
+        }).catch(function(error){
+            console.log("Error reading " + cur_path + " : " + error);
+            return "Error reading " + cur_path + " : " + error;
+        })
+    }
+}
+
+let arr = [];
+get_directory(["Computer"]);
+get_directory(["Computer","Z:","GEMINI-SYNCOPE"]);
+router.post('/', function(req, res, next) {
+    console.log(req.body["path"]);
+    if (req.body["path"].length === 1){
+        let res_json = {
+            "filename": req.body["path"][req.body["path"].length - 1],
+            "Root": [],
+            "Type": "Folder"
+        };
+        let cur_promise = new Promise(function (resolve, reject) {
+            resolve(get_directory(req.body["path"]))});
+        cur_promise.then(function(result){
+            res_json["Children"] = result;
+            res.send(JSON.stringify(res_json));
+        });
+    } else {
+        cur_path = path.normalize(path.join(...req.body["path"].slice(1)));
+        console.log(cur_path);
+        let res_json = {
+            "filename": req.body["path"][req.body["path"].length - 1],
+            "Root": req.body["path"],
+            "Type": fs.statSync(path.join(cur_path)).isDirectory() ? "Folder" : "File",
+        };
+        let cur_promise = new Promise(function (resolve, reject) {
+            resolve(get_directory(req.body["path"]))});
+        cur_promise.then(function(result){
+            res_json["Children"] = result;
+            res.send(JSON.stringify(res_json));
+        });
+    }
+
 });
 
+
 router.get('/', function(req, res, next) {
-    res_json = { "filename": "Current Folder",
-                "Root": "Full\\Path\\to\\Folder",
-                "Type": "Folder",
-                "Children": [
-                    {
-                        "filename": "Child of Current Folder",
-                        "Root": "Full\\Path\\to\\Folder\\Child of Current Folder",
-                        "Type": "File",
-                        "Children": []
-                    }
-
-                ]};
-     res.send(JSON.stringify(res_json));
-
+    let res_json = {
+        "filename": "Computer",
+        "Root": [],
+        "Type": "Folder"
+    };
+    let cur_promise = new Promise(function (resolve, reject) {
+        resolve(get_directory(["Computer"]));
+    });
+    cur_promise.then(function(result){
+        res_json["Children"] = result;
+        res.send(JSON.stringify(res_json));
+    });
 });
 
 module.exports = router;
