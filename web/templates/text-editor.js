6f66e3b1e27a7a5bedc5ff5e056825cc1e2f6782
shaun-mathew <shaunmathew1@hotmail.com>
2018-06-04 11:31:03 -0400

    More main simple functionality. Can specify python file (sorta) for arbitrary functions

--- web/templates/text-editor.js
+++ web/templates/text-editor.js
@@ -1,3 +1,6 @@
 var editor = ace.edit("editor-container");
+editor.setOptions({
+    useWrapMode: true
+})
 document.getElementById('editor-container').style.fontSize='20px';
 console.log("In text-editor")
