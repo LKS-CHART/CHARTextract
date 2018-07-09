b6df505f45b908fcd6015ba8e77846db3d50ebee
shaun-mathew <shaunmathew1@hotmail.com>
2018-07-09 16:35:52 -0400

    Increased visual contrast between selected item and recurring item

--- python_connector/connector.js
+++ python_connector/connector.js
@@ -3,8 +3,8 @@ process.env.PATH = process.env.VIRTUAL_ENV + ";" + process.env.PATH;
 let path = require('path');
 let pyshell = require('python-shell');
 
-pyshell.defaultOptions = { pythonPath: 'C:\\Users\\joshiu\\python\\venvs\\36-numpy\\Scripts\\python',
-    scriptPath: 'C:\\Users\\joshiu\\PyCharmProjects\\NgramRegexNLP'};
+pyshell.defaultOptions = { pythonPath: 'C:\\Users\\MathewSh\\anaconda3\\python',
+    scriptPath: 'C:\\Users\\MathewSh\\PyCharmProjects\\NgramRegexNLP'};
 
 pyshell = new pyshell('__main_simple__.py', {
     mode: 'json'
